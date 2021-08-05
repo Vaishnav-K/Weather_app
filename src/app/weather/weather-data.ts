10989c6ade2df2015063d9440d6f46ec445a81c8
Vaishnav-K <76683304+Vaishnav-K@users.noreply.github.com>
2021-08-05 09:12:41 +0530

    Update weather-data.ts

--- src/app/weather/weather-data.ts
+++ src/app/weather/weather-data.ts
@@ -1,5 +1,5 @@
 import {WeatherItem} from "./weather";
 export const WEATHER_ITEMS: WeatherItem[]=[
-    new WeatherItem('Lahore','Rainy',32),
-    new WeatherItem('Islamabad','Rainy',32)
+    new WeatherItem('Chennai','Sunny',32),
+    new WeatherItem('Mumbai','Rainy',32)
 ];
